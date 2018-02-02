6baedb979871c95edb2c738a68f52b6735640879
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2018-02-02 13:31:16 +0530

    Creating PostOffice class and starting to work skeletally on MainScraper. Still figuring out if my design is not terrible, and trying my best to document as I go.

---
+++ PostOffice.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen as op
+from bs4 import BeautifulSoup as soup
+
+class PostOffice:
+    url = "http://www.citypincode.co.in/"
+
+#TODO
+#Check database, see if the pincode is already loaded in the universal database
+
+    def pincode_already_captured(self):
+        return False
+
+
+    #Loads the html
+    def get_site_html(pincode, self):
+            if not self.pincode_already_captured:
+                myURL = PostOffice.url+pincode
+                page_html = op(myURL).read()
+                return page_html
+            else:
+                return None
+
+#to be filled in to check the validity of the pincode on the page
+#TODO
+    def is_valid_pincode(self):
+        return False
+
+#Gets the name of the district once given a valid post office URL whose
+#pincode has not yet been captured.
+    def getDistrict(self):
+        if self.is_valid_pincode():
+
+
--- TableGetter.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen as op
-from bs4 import BeautifulSoup as bs
-
-url = "www.citypincode.co.in/"
-
-
-
